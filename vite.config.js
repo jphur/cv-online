c0880b80c219464b0c4782d005718c54164c8f7d
Jorge Penadés <jorgepenades97@gmail.com>
2025-10-25 11:57:54 +0200

    chore: update version to 3.1.0 and add sass dependency
    fix: refactor vite.config.js for improved SCSS handling and add server configuration

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "src/assets/style.scss";`
-      }
-    }
-  }
-})
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("", import.meta.url)),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern-compiler",
+                additionalData: `@use "src/assets/style.scss" as *;`,
+            },
+        },
+    },
+    base: "/",
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        watch: { usePolling: true },
+        hmr: { host: "localhost" },
+    },
+});
