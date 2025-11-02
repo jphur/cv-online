4195eb05c3ad58feab3c62c58eb41757281f3960
Jorge Penadés <jorgepenades97@gmail.com>
2025-11-02 19:46:30 +0100

    add margin class

--- src/modules/hero.vue
+++ src/modules/hero.vue
@@ -3,7 +3,7 @@
         <div class="hero__content full-width text-center text-green" :class="$q.screen.gt.xs ? 'q-px-xl' : 'q-px-sm'">
             <p class="typewriter q-mb-sm">Desarrollador Full Stack</p>
             <transition appear enter-active-class="fadeInUp">
-                <h1 class="hero__name text-bold">Jorge Penadés Hurtado</h1>
+                <h1 class="hero__name text-bold q-mb-sm">Jorge Penadés Hurtado</h1>
             </transition>
             <transition appear enter-active-class="fadeInDown">
                 <h2 class="hero__tagline text-bold">Full Stack Web developer — IA & Prompt Engineering</h2>
@@ -19,7 +19,6 @@
             color: $lightest-slate;
             font-size: 4.5rem;
             line-height: 1.1;
-            margin-bottom: 10px;
         }
 
         &__tagline {
