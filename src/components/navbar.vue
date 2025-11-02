3f71cd66cc4fcfb8d5e9c0cfc56ed0057ee571bc
Jorge Penadés <jorgepenades97@gmail.com>
2025-11-02 17:31:33 +0100

    feat: update social media SVG icons and refactor navbar component for improved structure and styling

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -16,7 +16,7 @@
     ];
 </script>
 <template>
-    <nav class="navbar fixed-top row justify-between items-center q-px-md">
+    <q-toolbar class="navbar fixed-top z-max row justify-between items-center q-px-md">
         <div>
             <q-btn flat round color="green" icon="menu" class="lt-lg">
                 <q-menu>
@@ -24,7 +24,7 @@
                         <q-item v-for="item in navItems" :key="item.index" clickable v-ripple :href="`#${item.id}`" v-close-popup>
                             <q-item-section>
                                 <div class="navbar__link">
-                                    <span class="q-mr-xs text-green">{{ item.number }}</span>
+                                    <span class="q-mr-xs text-green" v-text="item.number" />
                                     {{ item.label }}
                                 </div>
                             </q-item-section>
@@ -36,7 +36,7 @@
                 <q-item v-for="item in navItems" :key="item.index" clickable v-ripple :href="`#${item.id}`" class="q-pa-none">
                     <q-item-section>
                         <div class="navbar__link q-px-sm">
-                            <span class="q-mr-xs text-green">{{ item.number }}</span>
+                            <span class="q-mr-xs text-green" v-text="item.number" />
                             {{ item.label }}
                         </div>
                     </q-item-section>
@@ -44,30 +44,24 @@
             </q-list>
         </div>
         <div>
-            <q-btn flat href="https://www.linkedin.com/in/jorgepenadeshurtado/" target="_blank" class="q-mx-xs" aria-label="LinkedIn" dense>
-                <q-img src="/svg/linkedin.svg" alt="LinkedIn" class="navbar__social-icon" width="1rem" />
+            <q-btn flat href="https://www.linkedin.com/in/jorgepenadeshurtado/" target="_blank" class="q-mr-sm" aria-label="LinkedIn" dense>
+                <q-img src="/svg/linkedin.svg" alt="LinkedIn" width="1rem" />
             </q-btn>
-            <q-btn flat href="https://github.com/jphur" target="_blank" class="q-mx-xs" aria-label="GitHub" dense>
-                <q-img src="/svg/github.svg" alt="GitHub" class="navbar__social-icon" width="1rem" />
+            <q-btn flat href="https://github.com/jphur" target="_blank"  aria-label="GitHub" dense>
+                <q-img src="/svg/github.svg" alt="GitHub" width="1rem" />
             </q-btn>
         </div>
-    </nav>
+    </q-toolbar>
 </template>
 <style lang="scss" scoped>
     .navbar {
         height: 4.5rem;
         background-color: $main;
         backdrop-filter: blur(0.6rem);
-        z-index: 100;
-
-        &__social-icon {
-            filter: brightness(0) saturate(100%) invert(81%) sepia(8%) saturate(1216%) hue-rotate(167deg) brightness(92%) contrast(88%);
-            transition: filter 0.25s cubic-bezier(0.645, 0.045, 0.355, 1);
-        }
 
         &__link {
             color: $lightest-slate;
-            transition: color .25s;
+            transition: color 0.25s;
 
             &:hover {
                 color: $green;
